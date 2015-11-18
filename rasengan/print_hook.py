7e99a5410f8e4dc8f4fd3a3df70d2108f3de87f2
pushpendre <pushpendre@jhu.edu>
2015-11-18 17:21:40 -0500

    Make the printhook class a singleton.

--- rasengan/print_hook.py
+++ rasengan/print_hook.py
@@ -3,25 +3,21 @@
 | Description : A hook to override stdin and stdout.
 | Author      : Pushpendre Rastogi
 | Created     : Wed Nov 18 16:19:20 2015 (-0500)
-| Last-Updated: Wed Nov 18 16:51:14 2015 (-0500)
+| Last-Updated: Wed Nov 18 17:20:45 2015 (-0500)
 |           By: Pushpendre Rastogi
-|     Update #: 6
+|     Update #: 14
 '''
 import sys
 
 class PrintHook(object):
     ''' this class gets all output directed to stdout(e.g by
     print statements) and stderr and redirects it to a user defined function
+    out, func and origOut would be delay initialized on Start.
     '''
-
-    def __init__(self):
-        ''' out, func and origOut would be delay initialized on Start.
-        '''
-        self.override = None
-        # func would be delayed initialized at start to a function
-        # that is called on the text to be printed.
-        self.func = None
-        self.origOut = None
+    override = None
+    func = None
+    origOut = None
+    already_started = 0
 
     @staticmethod
     def default(text):
@@ -36,18 +32,23 @@ class PrintHook(object):
         return text
 
     def start(self, func=None, override='stdout'):
-        if override == 'stdout':
-            sys.stdout.flush()
-            self.origOut = sys.stdout
-            sys.stdout = self
+        if self.already_started:
+            # If we have already overridden an output, then don't
+            # overide something else till restore the original back.
+            return self
         else:
-            sys.stderr.flush()
-            self.origOut = sys.stderr
-            sys.stderr= self
-        self.override = override
-        self.func = (func
-                     if func
-                     else self.TestHook)
+            if override == 'stdout':
+                sys.stdout.flush()
+                self.origOut = sys.stdout
+                sys.stdout = self
+            else:
+                sys.stderr.flush()
+                self.origOut = sys.stderr
+                sys.stderr= self
+            self.override = override
+            self.func = (func
+                         if func
+                         else PrintHook.default)
         return self
 
     def stop(self):
@@ -56,7 +57,7 @@ class PrintHook(object):
         else:
             sys.stderr = self.origOut
         self.func = None
-
+        self.already_started = 0
 
     def write(self, text):
         processed_text = self.func(text)
@@ -66,4 +67,5 @@ class PrintHook(object):
     def __getattr__(self, name):
         ''' delegate all other methods to origOut so that we
         don't have to override them'''
-        return self.origOut.__getattr__(name)
+
+        return getattr(self.origOut, name)
